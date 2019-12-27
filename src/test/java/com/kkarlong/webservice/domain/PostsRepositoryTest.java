58c7ac141dc18d08095ef08ee5aca2e2377e949a
ghghrna <jeongwan.hong92@gmail.com>
2019-12-27 14:17:34 +0900

    add domain and repository

---
+++ src/test/java/com/kkarlong/webservice/domain/PostsRepositoryTest.java
@@ -0,0 +1,44 @@
+package com.kkarlong.webservice.domain;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void savePostsAndReadOne() {
+        //given
+        postsRepository.save(Posts.builder()
+                .title("테스트 제목")
+                .content("테스트 본문")
+                .author("kkarlong")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle(), is("테스트 제목"));
+        assertThat(posts.getContent(), is("테스트 제목"));
+    }
+}
