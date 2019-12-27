6e33e92101d8a098c9e0fa344fb579d41ae75826
ghghrna <jeongwan.hong92@gmail.com>
2019-12-27 13:45:41 +0900

    Create Project

---
+++ src/test/java/com/kkarlong/webservice/NewSpringWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkarlong.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewSpringWebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
